141f580343838b372555549be2b8c600ebbbd08e
yinjiahao <1497160501@qq.com>
2024-12-05 16:45:25 +0800

    数据结构概述

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct student{
-    int num;
-    char name[20];
-    char sex;
-}stu,*pStu;
-
+typedef struct Lnode{
+    int data;
+    struct Lnode *next;
+}Lnode,*LinkList;
 int main() {
-    stu s;
-    scanf("%d%s %c",&s.num,s.name,&s.sex);
-    printf("num:%d,name:%s,sex:%c",s.num,s.name,s.sex);
+    Lnode *L;
+    L=(LinkList) malloc(sizeof(Lnode));
+    return 0;
 }
