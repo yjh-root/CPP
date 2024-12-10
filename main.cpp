7e63cc92770fe5f655a572d771e6e6e89f739cae
yinjiahao <1497160501@qq.com>
2024-12-10 15:52:41 +0800

    链表

--- main.cpp
+++ main.cpp
@@ -3,87 +3,64 @@
 
 #define maxSize 50
 typedef int ElemType;//让顺序表存储其他类型元素时，可以快速完成代码修改
-typedef struct {
+typedef struct{
     ElemType data[maxSize];
-    int len;//顺序表长度
+    int len;
 }sqList;
-//顺序表的插入，因为L会改变，因此我们这里要引用,i是插入的位置
-bool ListInsert(sqList &L,int i,ElemType elemType){
-    //判断i是否合法
-    if(1>i||i>L.len+1){
+bool insertValue(sqList &L,ElemType value,int pos){
+    if(1>pos||pos>L.len+1){
+        printf("false\n");
         return false;
     }
-    //如果存储空间满了，不能插入
     if(L.len==maxSize){
+        printf("false\n");
         return false;
     }
-    //把后面的元素依次往后移动，空出位，放入要插入的元素
-    for(int j=L.len;j>=i;j--){
+    for (int j = L.len; j >=  pos; j--) {
         L.data[j]=L.data[j-1];
     }
-    L.data[i-1]=elemType;//放入要插入的元素
-    L.len++;//顺序表长度加1
-    return true;
-}
-//打印顺序表
-void PrintList(sqList L){
+    L.data[pos-1]=value;
+    L.len++;
     for (int i = 0; i < L.len; ++i) {
         printf("%3d",L.data[i]);
     }
-}
-//删除顺序表中的元素,i是要删除的元素的位置，del是为了获取被删除的元素的值
-bool ListDelete(sqList &L,int i,ElemType &del){
-    //判断删除的元素的位置是否合法
-    if(i<1||i>L.len)
-        return false;
-    del=L.data[i-1];//首先保存要删除元素的值
-    for (int j = i-1; j <L.len ; ++j)
-        L.data[j-1]=L.data[j];
-    L.len--;
     return true;
-
 }
-//查找某个元素的位置，找到了就返回对应位置，没找到就就返回0
-int LocateElem(sqList L,ElemType elemType){
-    for (int i = 0; i < L.len; ++i) {
-        if(elemType==L.data[i])
-            return i+1;
+bool deletePost(sqList &L,int delPos){
+    if(delPos>L.len){
+        printf("false\n");
+        return false;
     }
-    return 0;
+    for (int i = delPos-1; i < L.len; ++i) {
+        L.data[i-1]=L.data[i];
+    }
+    L.len--;
+    for (int i = 0; i < L.len; ++i) {
+        printf("%3d",L.data[i]);
+    }
+    return true;
 }
 int main() {
-    sqList L;//定义一个顺序表
-    bool ret;//ret用来装函数的返回值
-    L.data[0]=1;//放入元素
+    sqList L;
+    L.data[0]=1;
     L.data[1]=2;
     L.data[2]=3;
-    L.len=3;//设置长度
-    ElemType del;//删除的元素存入del中
-    int pos;//存储元素位置
+    L.len=3;
     for (int i = 0; i < L.len; ++i) {
         printf("%3d",L.data[i]);
     }
     printf("\n");
-    ret =ListInsert(L,2,60);
-    if(ret){
-        printf("insert sqList success\n");
-        PrintList(L);
-    } else
-        printf("insert sqList failed\n");
+    fflush(stdout);
+    ElemType value;
+    int pos=2;
+    scanf("%d",&value);
+    fflush(stdout);
+    insertValue(L,value,pos);
     printf("\n");
-    ret =ListDelete(L,1,del);
-    if(ret){
-        printf("delete sqList success\n");
-        printf("delete element=%d\n",del);
-        PrintList(L);
-    } else
-        printf("delete sqList failed\n");
-    pos=LocateElem(L,60);
-    if(pos){
-        printf("find this element\n");
-        printf("element pos=%d\n",pos);
-    } else{
-        printf("find this element\n");
-    }
+    int delPos;
+    fflush(stdout);
+    scanf("%d",&delPos);
+    fflush(stdout);
+    deletePost(L,delPos);
     return 0;
 }
