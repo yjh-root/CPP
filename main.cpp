9028054d3722d67d4c044815e6115749f10a59ce
yinjiahao <1497160501@qq.com>
2025-03-31 17:16:32 +0800

    浮点数

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 #include <time.h>
 #include <string>
 
-//补码实战
+//精度丢失
 int main() {
-    int i,j,k;
-    i=-5;
-    j=2;
-    k=j+i;
-    printf("k=%d",k);
+    //赋值的一瞬间发生精度丢失，因为浮点数常量默认是8个字节存储float型
+    float a=1.23456789e10,b;//12345678900
+    double c=1.23456789e10,d,f;//12345678900
+    //理论上结果应该是12345678920
+    b=a+20;//精度丢失
+    d=c+20;//精度未丢失
+    f=a+20;//精度丢失
+    printf("b=%f\n",b);//%f即可输出float，也可输出double
+    printf("d=%f\n",d);//%f即可输出float，也可输出double
+    printf("f=%f\n",f);//%f即可输出float，也可输出double
+    return 0;
 }
