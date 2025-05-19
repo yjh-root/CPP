23f476105c8b47d8297e2cbb725d2e67b56ce5fd
YinJiahao <yin_jia_hao@outlook.com>
2025-05-19 14:28:13 +0800

    线性表

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
-void loveYou(int n){
-    for (int i = 1; i <= n; ++i) {
-        printf("I Love you %d num\n",i);
-    }
-    printf("I Love you more than %d num\n",n);
-}
-//文件操作
+
+
 int main() {
-    loveYou(1000);
-    return 0;
+     return 0;
 }
